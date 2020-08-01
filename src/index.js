bf66af68eefe558cde30b40f97ec1ea117d8ad0d
jamescopping <james.copping97@gmail.com>
2020-08-01 13:33:43 +0100

    simple layout

--- src/index.js
+++ src/index.js
@@ -1,14 +1,55 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
-function textArea() {
 
-  const testarea = document.createElement('textarea');
+function adventurePage() {
+  const main = document.createElement("main");
+  main.setAttribute("role", "main");
+  main.className = "bg-dark p-3";
+  const container = document.createElement("div");
+  container.className = "full-height";
 
-  return testarea;
+
+  container.appendChild(adventureLog());
+  container.appendChild(commandArea());
+  main.appendChild(container);
+
+  return main;
 }
 
-document.body.appendChild(textArea());
+function commandArea() {
+
+  const footer = document.createElement('footer');
+  footer.className = "footer fixed-bottom p-3";
+
+
+  const inputGroup = document.createElement("div");
+  inputGroup.className = "input-group input-group-lg";
+  const placeholder = "Type your command or /help for the list of commands you can do";
+
+  inputGroup.innerHTML += `<input type="text" id="command-input" placeholder="${placeholder}" class="form-control shadow-none"></input>`;
+  inputGroup.innerHTML += `<div class="input-group-append">
+    <button class="btn btn-secondary" type="button" id="command-submit">Submit</button>
+  </div>`;
+
+  footer.appendChild(inputGroup);
+
+  return footer;
+}
+
+function adventureLog() {
+
+  const log = document.createElement("div");
+  log.className = "text-light";
+  log.textContent = "This is a test"
+  return log;
+}
+
+(() => {
+  document.body.appendChild(adventurePage());
+})();
+
+
 
 
 
