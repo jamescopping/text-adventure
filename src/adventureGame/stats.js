8c24f5803be53ae258d711fff4c8d09f28c5172f
jamescopping <james.copping97@gmail.com>
2020-09-19 15:41:20 +0100

    #53 display stats command

--- src/adventureGame/stats.js
+++ src/adventureGame/stats.js
@@ -10,6 +10,8 @@ export class Stats {
 		resourceObjectArray.forEach((resourceObject) => {
 			this.resourceList.push(new Resource(resourceObject));
 		});
+		this.buffSet = new Set();
+		this.debuffSet = new Set();
 		this.setStatus(MobStatus.ALIVE);
 		this.setInitiativeBonus(0);
 		this.setArmourClass(0);
@@ -22,13 +24,20 @@ export class Stats {
 		});
 	}
 
+	getBuffSet() { return this.buffSet }
+	getDebuffSet() { return this.debuffSet }
+	addBuff(buff) { this.buffSet.add(buff) }
+	removeBuff(buff) { this.buffSet.delete(buff) }
+	addDebuff(debuff) { this.debuffSet.add(debuff) }
+	removeDebuff(debuff) { this.debuffSet.delete(debuff) }
+
 	getInitiativeBonus() { return this.initiativeBonus }
 	setInitiativeBonus(value) { this.initiativeBonus = (value !== undefined) ? parseInt(value) : 0 }
 	getArmourClass() { return this.armourClass }
 	setArmourClass(value) { this.armourClass = (value !== undefined) ? parseInt(value) : 0 }
 	getAttackBonus() { return this.attackBonus }
 	setAttackBonus(value) { this.attackBonus = (value !== undefined) ? parseInt(value) : 0 }
-
 	getStatus() { return this.status }
 	setStatus(status) { this.status = status }
+	getResourceList() { return this.resourceList }
 }
